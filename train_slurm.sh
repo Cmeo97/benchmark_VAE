2902ca140c4b77727e696a1acc55f080111ad39d
Cmeo97 <cristianmeo2@gmail.com>
2023-01-12 14:18:02 +0100

    hpc bash file

--- train_slurm.sh
+++ train_slurm.sh
@@ -16,18 +16,19 @@ conda activate MARL
 
 dataset=$1
 model_name=$2
-update_architecture=$3
+latent_dim=$3
 seed=$4
 beta=$5
 alpha=$6
 C=$7
-latent_dim=$8
+enc_celeba=$8
+dec_celeba=$9
 
 
-ExpName=${model_name}"_"${dataset}"_"${seed}"_"${update_architecture}"_"${beta}"_"${alpha}"_"${C}"_"${latent_dim}
-echo "doing experiment: ${ExpName}"
+ExpName=${model_name}"_"${dataset}"_"${seed}"_"${beta}"_"${alpha}"_"${C}"_"${latent_dim}"_"${enc_celeba}"_"${dec_celeba}
+echo "Training of experiment: ${ExpName}"
 
-python examples/scripts/training.py \
+nohup python examples/scripts/training.py \
 --dataset=${dataset} \
 --model_name=${model_name} \
 --model_config=/home/cristianmeo/benchmark_VAE/examples/scripts/configs/${dataset}/${model_name}_config.json \
@@ -41,6 +42,20 @@ python examples/scripts/training.py \
 --name_exp=${ExpName} \
 --data_path=$DATA_PATH \
 --use_hpc \
+--enc_celeba=${enc_celeba} \
+--dec_celeba=${dec_celeba} \
 > logs/${ExpName}.out 2> logs/${ExpName}.err 
+echo "Evaluation of experiment: ${ExpName}"
+python examples/scripts/validation.py \
+--dataset=${dataset} \
+--model_name=${model_name} \
+--model_config=/home/cristianmeo/benchmark_VAE/examples/scripts/configs/${dataset}/${model_name}_config.json \
+--training_config=/home/cristianmeo/benchmark_VAE/examples/scripts/configs/${dataset}/base_training_config.json \
+--exp_name=${ExpName} \
+--data_path=$DATA_PATH \
+--enc_celeba=${enc_celeba} \
+--dec_celeba=${dec_celeba} \
+--use_hpc \
+
 
 
